b19dfab3d0dd9c099368444d87750687b350a51a
rbenzecry <rbenzecry97@gmail.com>
2024-03-27 17:04:28 +0000

    Renaming folders. Pending to reorganise order

---
+++ 02_household-surveys/05_rename-folders-geih.R
@@ -0,0 +1,46 @@
+
+library(tidyverse)
+library(readxl)
+library(haven)
+library(janitor)
+library(data.table)
+library(sjlabelled)
+library(sjPlot)
+
+rm(list = ls())
+
+# FUNCTIONS ---------------------------------------------------------------
+
+# Function to extract month name from directory name
+extract_month <- function(dir_name, months) {
+  name_month <- str_extract(dir_name, paste(months, collapse = "|"))
+  return(name_month)
+}
+
+# DATA --------------------------------------------------------------------
+
+months <- c("Enero", "Febrero", "Marzo", "Abril",
+            "Mayo", "Junio", "Julio", "Agosto", 
+            "Septiembre", "Octubre", "Noviembre", "Diciembre")
+
+survey_year <- "2022"
+initial_dir <- paste("Data/GEIH-", survey_year, sep = "")
+
+
+# List the directories in the parent directory
+directories <- list.dirs(path = initial_dir, full.names = TRUE, recursive = FALSE)
+
+
+folders_names <- list.files(initial_dir)
+
+
+# Apply the function to each directory name
+new_names <- sapply(directories, 
+                    function(x) extract_month(dir_name = x,
+                                              months = months))
+
+# Rename the directories
+file.rename(from = directories, 
+            to = paste0(dirname(directories), "/", new_names))
+
+# -------------------------------------------------------------------------
